6671a194f455229fc54a089410e56c531603d708
AlexandraSpeshilova <165372876+speshilovaAlexandra@users.noreply.github.com>
2026-06-17 17:33:58 +0500

    Update AdminFlowers.vue

--- src/views/admin/AdminFlowers.vue
+++ src/views/admin/AdminFlowers.vue
@@ -76,15 +76,14 @@
             </svg>
             <div v-else class="upload-spinner"></div>
             <span>{{ uploading ? 'Загрузка...' : 'Загрузить файл' }}</span>
-            <!-- В csv-actions добавить -->
-            <button @click="downloadTemplate" class="btn-sample">
+          </button>
+          <button @click="downloadTemplate" class="btn-sample">
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
                 <polyline points="7 10 12 15 17 10"/>
                 <line x1="12" y1="15" x2="12" y2="3"/>
               </svg>
               Скачать шаблон
-            </button>
           </button>
           
 
